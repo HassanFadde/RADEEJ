fbf6cfa3bd8f5dd6d6701b501e101627803cdd2f
unknown <you@example.com>
2021-09-30 14:55:10 +0100

    binding form's entries

--- main.py
+++ main.py
@@ -69,7 +69,10 @@ class MyApp:
         self.clean_window()
         self.create_form()
     def create_form(self):
-
+        self.current_focus_entry=1
+        
+        self.entries_form=[] # 0->entry_consommation_RADEEJ 1->entry_prix_RADEEJ 2->entry_etat_initial_compteur 3->entry_etat_final_compteur 4->entry_etat_initial_compteur_Mohcine 5->entry_etat_final_compteur_Mohcine 6->entry_etat_initial_compteur_Mustafa 7->entry_etat_final_compteur_Mustafa
+        
         self.create_title("insérer vos informations",10)
         
         frame_form=Frame(self.canvas)
@@ -78,20 +81,20 @@ class MyApp:
         label_consommation_RADEEJ=Label(frame_RADEEJ,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Consommation donnée par la RADEEJ : ",width=40)
         label_consommation_RADEEJ.grid(row=0,column=0,padx=20,pady=20)
         frame_RADEEJ_cons=Frame(frame_RADEEJ)
-        self.entry_consommation_RADEEJ=Entry(frame_RADEEJ_cons,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_consommation_RADEEJ.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_RADEEJ_cons,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[0].grid(row=0,column=0)
         label_m_RADEEJ=Label(frame_RADEEJ_cons,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_RADEEJ.grid(row=0,column=1)
         frame_RADEEJ_cons.grid(row=0,column=1,padx=10)
+        
         #new line
+        
         label_prix_RADEEJ=Label(frame_RADEEJ,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Prix donné par la RADEEJ : ",width=40)
         label_prix_RADEEJ.grid(row=1,column=0,padx=20)
         frame_RADEEJ_prix=Frame(frame_RADEEJ)
-        self.entry_prix_RADEEJ=Entry(frame_RADEEJ_prix,font=(self.text_familly,self.label_text_size,self.font_style),fg="grey",width=25)
-        self.entry_prix_RADEEJ.insert(0,"si possible")
-        self.entry_prix_RADEEJ.bind("<FocusIn>",self.focus_in_prix)
-        self.entry_prix_RADEEJ.bind("<FocusOut>",self.focus_out_prix)
-        self.entry_prix_RADEEJ.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_RADEEJ_prix,font=(self.text_familly,self.label_text_size,self.font_style),fg="grey",width=25))
+        self.entries_form[1].insert(0,"si possible")
+        self.entries_form[1].grid(row=0,column=0)
         label_DHs_RADEEJ=Label(frame_RADEEJ_prix,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" DHs ")
         label_DHs_RADEEJ.grid(row=0,column=1)
         frame_RADEEJ_prix.grid(row=1,column=1,padx=10)
@@ -104,17 +107,19 @@ class MyApp:
         label_etat_initial=Label(frame_compteur,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur : ",width=40)
         label_etat_initial.grid(row=0,column=0,padx=20,pady=20)
         frame_compteur_initial=Frame(frame_compteur)
-        self.entry_etat_initial_compteur=Entry(frame_compteur_initial,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_initial_compteur.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_initial,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[2].grid(row=0,column=0)
         label_m_etat_initial=Label(frame_compteur_initial,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial.grid(row=0,column=1)
         frame_compteur_initial.grid(row=0,column=1,padx=10)
+        
         #new line
+        
         label_etat_final=Label(frame_compteur,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur : ",width=40)
         label_etat_final.grid(row=1,column=0,padx=20)
         frame_compteur_final=Frame(frame_compteur)
-        self.entry_etat_final=Entry(frame_compteur_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_final.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[3].grid(row=0,column=0)
         label_m_etat_final=Label(frame_compteur_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_final.grid(row=0,column=1)
         frame_compteur_final.grid(row=1,column=1,padx=10)
@@ -127,17 +132,19 @@ class MyApp:
         label_etat_initial_Mohcine=Label(frame_compteur_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur de Mohcine : ",width=40)
         label_etat_initial_Mohcine.grid(row=0,column=0,padx=20,pady=20)
         frame_compteur_initial_Mohcine=Frame(frame_compteur_Mohcine)
-        self.entry_etat_initial_compteur_Mohcine=Entry(frame_compteur_initial_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_initial_compteur_Mohcine.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_initial_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[4].grid(row=0,column=0)
         label_m_etat_initial_Mohcine=Label(frame_compteur_initial_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial_Mohcine.grid(row=0,column=1)
         frame_compteur_initial_Mohcine.grid(row=0,column=1,padx=10)
+        
         #new line
+        
         label_etat_final_Mohcine=Label(frame_compteur_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur de Mohcine : ",width=40)
         label_etat_final_Mohcine.grid(row=1,column=0,padx=20)
         frame_compteur_Mohcine_final=Frame(frame_compteur_Mohcine)
-        self.entry_etat_final_Mohcine=Entry(frame_compteur_Mohcine_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_final_Mohcine.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_Mohcine_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[5].grid(row=0,column=0)
         label_m_etat_final_Mohcine=Label(frame_compteur_Mohcine_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_final_Mohcine.grid(row=0,column=1)
         frame_compteur_Mohcine_final.grid(row=1,column=1,padx=10)
@@ -150,17 +157,19 @@ class MyApp:
         label_etat_initial_Mustafa=Label(frame_compteur_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur de Mustafa : ",width=40)
         label_etat_initial_Mustafa.grid(row=0,column=0,padx=20,pady=20)
         frame_compteur_initial_Mustafa=Frame(frame_compteur_Mustafa)
-        self.entry_etat_initial_compteur_Mustafa=Entry(frame_compteur_initial_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_initial_compteur_Mustafa.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_initial_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[6].grid(row=0,column=0)
         label_m_etat_initial_Mustafa=Label(frame_compteur_initial_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial_Mustafa.grid(row=0,column=1)
         frame_compteur_initial_Mustafa.grid(row=0,column=1,padx=10)
+        
         #new line
+        
         label_etat_final_Mustafa=Label(frame_compteur_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur de Mustafa : ",width=40)
         label_etat_final_Mustafa.grid(row=1,column=0,padx=20)
         frame_compteur_Mustafa_final=Frame(frame_compteur_Mustafa)
-        self.entry_etat_final_Mustafa=Entry(frame_compteur_Mustafa_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25)
-        self.entry_etat_final_Mustafa.grid(row=0,column=0)
+        self.entries_form.append(Entry(frame_compteur_Mustafa_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.entry_text_color,width=25))
+        self.entries_form[7].grid(row=0,column=0)
         label_m_etat_final_Mustafa=Label(frame_compteur_Mustafa_final,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_final_Mustafa.grid(row=0,column=1)
         frame_compteur_Mustafa_final.grid(row=1,column=1,padx=10)        
@@ -168,18 +177,33 @@ class MyApp:
 
         label_space=Label(frame_form,text="")
         label_space.pack()
-
         self.canvas.create_window(self.width/2,self.height*9/20+self.padding*4/5,window=frame_form)
-    #bind
-    def focus_in_prix(self,*args)->None:
-        if self.entry_prix_RADEEJ.get()=="si possible":
-            self.entry_prix_RADEEJ.delete(0,END)
-            self.entry_prix_RADEEJ.config(fg=self.entry_text_color)
-    def focus_out_prix(self,*args)->None:
-        if not self.entry_prix_RADEEJ.get() or not self.can_it_be_number(self.entry_prix_RADEEJ.get()):
-            self.entry_prix_RADEEJ.delete(0,END)
-            self.entry_prix_RADEEJ.config(fg="grey")
-            self.entry_prix_RADEEJ.insert(0,"si possible")
+        
+        #bind the enties
+        
+        self.entries_form[1].bind("<FocusIn>",self.focus_in_entry)
+        self.entries_form[1].bind("<FocusOut>",self.focus_out_entry)    
+    #bind's functions
+    def focus_in_entry(self,*args)->None:
+        print(args)
+        if self.current_focus_entry<0 and self.current_focus_entry>=len(self.entries_form):
+            return
+        if not self.can_it_be_number(self.entries_form[self.current_focus_entry].get()):
+            self.entries_form[self.current_focus_entry].delete(0,END)
+            self.entries_form[self.current_focus_entry].config(fg=self.entry_text_color)
+    def focus_out_entry(self,*args)->None:
+        if self.current_focus_entry<0 and self.current_focus_entry>=len(self.entries_form):
+            return
+        if not self.entries_form[self.current_focus_entry].get() or not self.can_it_be_number(self.entries_form[self.current_focus_entry].get()):
+            self.entries_form[self.current_focus_entry].delete(0,END)
+            if self.current_focus_entry==1:
+                self.entries_form[self.current_focus_entry].config(fg="grey")
+                self.entries_form[self.current_focus_entry].insert(0,"si possible")
+        self.current_focus_entry=-1
+    def up_entry(self,*args):
+        pass
+    def down_entry(self,*args):
+        pass
     #other
     def can_it_be_number(self,string_to_check:str)->bool:
         my_set=set("1 2 3 4 5 6 7 8 9 . ,")
