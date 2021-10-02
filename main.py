f4240ebd37149601edfb65504ed638cf40dba737
HassanFadde <76838727+HassanFadde@users.noreply.github.com>
2021-10-02 18:13:25 +0100

    add date's combobox

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
+import datetime
 from PIL import ImageTk , Image
 class MyApp:
     def __init__(self):
         #size window
-        self.width=1028
-        self.height=720
+        self.width=1100
+        self.height=self.width*720//1028
         self.title_size=50
-        self.text_size=30
+        self.text_M_size=30
+        self.text_C_size=17
         self.label_text_size=15
         self.padding=100
         #title
@@ -16,16 +19,18 @@ class MyApp:
         self.logo_path="images/Cytro.ico"
         self.image_path=""
         #color 
+        self.button_C_background_color="light blue"
+        self.button_C_color="grey"
         self.title_color="#55ffff"
         self.border_color="light blue"
         self.title_font_style="italic bold"
         self.font_style="italic"
         self.background_color="grey"
-        self.button_background_color="pink"
+        self.button_M_background_color="pink"
         self.label_text_color="black"
         self.entry_text_color="black"
         self.label_background_color="light blue"
-        self.button_color="black"
+        self.button_M_color="black"
         self.text_familly="matey"
         #window
         self.window=Tk()
@@ -33,6 +38,7 @@ class MyApp:
         self.window.iconbitmap(self.logo_path)
         self.window.geometry(f"{self.width}x{self.height}")
         self.window.resizable(False,False)
+        self.data_entries=[]
         self.create_canvas()
         self.create_menu()
         self.window.mainloop()
@@ -44,38 +50,29 @@ class MyApp:
             bg=ImageTk.PhotoImage(bg)
             self.canvas.create_image(0,0,image=bg,anchor="nw")
         self.canvas.pack()
-    def create_button(self,text:str,this_width:int,this_command="")->Button:
-        return Button(self.canvas,text=text,bg=self.button_background_color,fg=self.button_color,font=(self.text_familly,self.text_size,self.font_style),width=this_width,command=this_command)
     def create_title(self,this_text:str,height_porcentage:int)->None:
         self.canvas.create_text(self.width/2,self.height/height_porcentage,text=this_text,font=(self.text_familly,self.title_size,self.title_font_style),fill=self.title_color)
     def create_menu(self):
         # create title 
         self.create_title("Menu",5)
         #create buttons
-        self.button_calculer=self.create_button("Calculer",20,self.button_calculer)
-        self.button_historique=self.create_button("Historique",20)
-        self.canvas.create_window(self.width/2,self.height/5+self.padding,window=self.button_calculer)
-        self.canvas.create_window(self.width/2,self.height/5+2*self.padding,window=self.button_historique)
+        button_calculer=Button(self.canvas,text="Calculer",bg=self.button_M_background_color,fg=self.button_M_color,font=(self.text_familly,self.text_M_size,self.font_style),width=20,command=self.button_calculer)
+        button_historique=Button(self.canvas,text="Historique",bg=self.button_M_background_color,fg=self.button_M_color,font=(self.text_familly,self.text_M_size,self.font_style),width=20)
+        self.canvas.create_window(self.width/2,self.height/5+self.padding,window=button_calculer)
+        self.canvas.create_window(self.width/2,self.height/5+2*self.padding,window=button_historique)
         #create border
         self.canvas.create_line(self.width/2-100,self.height/5,self.width/2-300,self.height/5,fill=self.border_color,width=5)
         self.canvas.create_line(self.width/2-300,self.height/5,self.width/2-300,self.height/5+300,fill=self.border_color,width=5)
         self.canvas.create_line(self.width/2-300,self.height/5+300,self.width/2+300,self.height/5+300,fill=self.border_color,width=5)
         self.canvas.create_line(self.width/2+300,self.height/5+300,self.width/2+300,self.height/5,fill=self.border_color,width=5)
         self.canvas.create_line(self.width/2+300,self.height/5,self.width/2+100,self.height/5,fill=self.border_color,width=5)
-    def clean_window(self):
-        self.canvas.destroy()
-        self.create_canvas()
-    def button_calculer(self):
-        self.clean_window()
-        self.create_form()
     def create_form(self):
         self.current_focus_entry,self.next_focus_entry=-1,-1
+        self.index_focus_combobox,self.index_next_focus_combobox=-1,-1
         self.entries_form=[] # 0->entry_consommation_RADEEJ 1->entry_prix_RADEEJ 2->entry_etat_initial_compteur 3->entry_etat_final_compteur 4->entry_etat_initial_compteur_Mohcine 5->entry_etat_final_compteur_Mohcine 6->entry_etat_initial_compteur_Mustafa 7->entry_etat_final_compteur_Mustafa
-        
+        self.date=[]
         self.create_title("insérer vos informations",10)
-        
-        frame_form=Frame(self.canvas)
-
+        frame_form=Frame(self.canvas,bd=2,relief="raised")
         frame_RADEEJ=Frame(frame_form)
         label_consommation_RADEEJ=Label(frame_RADEEJ,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Consommation donnée par la RADEEJ : ",width=40)
         label_consommation_RADEEJ.grid(row=0,column=0,padx=20,pady=20)
@@ -85,9 +82,7 @@ class MyApp:
         label_m_RADEEJ=Label(frame_RADEEJ_cons,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_RADEEJ.grid(row=0,column=1)
         frame_RADEEJ_cons.grid(row=0,column=1,padx=10)
-        
         #new line
-        
         label_prix_RADEEJ=Label(frame_RADEEJ,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Prix donné par la RADEEJ : ",width=40)
         label_prix_RADEEJ.grid(row=1,column=0,padx=20)
         frame_RADEEJ_prix=Frame(frame_RADEEJ)
@@ -98,10 +93,8 @@ class MyApp:
         label_DHs_RADEEJ.grid(row=0,column=1)
         frame_RADEEJ_prix.grid(row=1,column=1,padx=10)
         frame_RADEEJ.pack(pady=5)
-        
         label_line_1=Label(frame_form,text="_____________________________________________________________")
         label_line_1.pack()
-        
         frame_compteur=Frame(frame_form)
         label_etat_initial=Label(frame_compteur,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur : ",width=40)
         label_etat_initial.grid(row=0,column=0,padx=20,pady=20)
@@ -111,7 +104,6 @@ class MyApp:
         label_m_etat_initial=Label(frame_compteur_initial,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial.grid(row=0,column=1)
         frame_compteur_initial.grid(row=0,column=1,padx=10)
-        
         #new line
         label_etat_final=Label(frame_compteur,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur : ",width=40)
         label_etat_final.grid(row=1,column=0,padx=20)
@@ -122,10 +114,8 @@ class MyApp:
         label_m_etat_final.grid(row=0,column=1)
         frame_compteur_final.grid(row=1,column=1,padx=10)
         frame_compteur.pack()
-        
         label_line_2=Label(frame_form,text="_____________________________________________________________")
         label_line_2.pack()
-
         frame_compteur_Mohcine=Frame(frame_form)
         label_etat_initial_Mohcine=Label(frame_compteur_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur de Mohcine : ",width=40)
         label_etat_initial_Mohcine.grid(row=0,column=0,padx=20,pady=20)
@@ -135,9 +125,7 @@ class MyApp:
         label_m_etat_initial_Mohcine=Label(frame_compteur_initial_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial_Mohcine.grid(row=0,column=1)
         frame_compteur_initial_Mohcine.grid(row=0,column=1,padx=10)
-        
         #new line
-        
         label_etat_final_Mohcine=Label(frame_compteur_Mohcine,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur de Mohcine : ",width=40)
         label_etat_final_Mohcine.grid(row=1,column=0,padx=20)
         frame_compteur_Mohcine_final=Frame(frame_compteur_Mohcine)
@@ -147,10 +135,8 @@ class MyApp:
         label_m_etat_final_Mohcine.grid(row=0,column=1)
         frame_compteur_Mohcine_final.grid(row=1,column=1,padx=10)
         frame_compteur_Mohcine.pack()
-
         label_line_3=Label(frame_form,text="_____________________________________________________________")
         label_line_3.pack()
-
         frame_compteur_Mustafa=Frame(frame_form)
         label_etat_initial_Mustafa=Label(frame_compteur_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat initial du compteur de Mustafa : ",width=40)
         label_etat_initial_Mustafa.grid(row=0,column=0,padx=20,pady=20)
@@ -160,9 +146,7 @@ class MyApp:
         label_m_etat_initial_Mustafa=Label(frame_compteur_initial_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg="white",relief="sunken",text=" m³ ")
         label_m_etat_initial_Mustafa.grid(row=0,column=1)
         frame_compteur_initial_Mustafa.grid(row=0,column=1,padx=10)
-        
         #new line
-        
         label_etat_final_Mustafa=Label(frame_compteur_Mustafa,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Etat final du compteur de Mustafa : ",width=40)
         label_etat_final_Mustafa.grid(row=1,column=0,padx=20)
         frame_compteur_Mustafa_final=Frame(frame_compteur_Mustafa)
@@ -172,11 +156,43 @@ class MyApp:
         label_m_etat_final_Mustafa.grid(row=0,column=1)
         frame_compteur_Mustafa_final.grid(row=1,column=1,padx=10)        
         frame_compteur_Mustafa.pack()
-
-        label_space=Label(frame_form,text="")
-        label_space.pack()
-        self.canvas.create_window(self.width/2,self.height*9/20+self.padding*4/5,window=frame_form)
-        self.data_entries=[-1 for _ in range(len(self.entries_form))]
+        if not self.data_entries :
+            self.data_entries=[-1 for _ in range(len(self.entries_form))]
+        else :
+            for index in range(len(self.data_entries)):
+                if self.data_entries[index]>=0:
+                    if index==1:
+                        self.entries_form[index].delete(0,END)
+                        self.entries_form[index].config(fg=self.entry_text_color)
+                    self.entries_form[index].insert(0,self.data_entries[index])
+        label_line_4=Label(frame_form,text="_____________________________________________________________")
+        label_line_4.pack()
+        #add ComboBox  date
+        self.yy,self.mm,self.dd=list(map(int,str(datetime.datetime.now()).split()[0].split('-')))
+        self.current_dd_mm_yy=[self.dd,self.mm,self.yy]
+        frame_date=Frame(frame_form)
+        label_date=Label(frame_date,font=(self.text_familly,self.label_text_size,self.font_style),fg=self.label_text_color,bg=self.label_background_color,relief="solid",text="Date (Jour/Mois/Année) : ",width=40)
+        label_date.grid(row=0,column=0,padx=20)
+        frame_date_combo_box=Frame(frame_date)
+        self.date.append(ttk.Combobox(frame_date_combo_box,value=["01","02","03","04","05","06","07","08","09"]+list(range(10,32)),font=(self.text_familly,self.label_text_size,self.font_style),width=7))
+        self.date[0].current(self.dd-1)
+        self.date[0].grid(row=0,column=0)
+        self.date.append(ttk.Combobox(frame_date_combo_box,value=["01","02","03","04","05","06","07","08","09","10","11","12"],font=(self.text_familly,self.label_text_size,self.font_style),width=8))
+        self.date[1].current(self.mm-1)
+        self.date[1].grid(row=0,column=1)
+        self.date.append(ttk.Combobox(frame_date_combo_box,value=[self.yy-1,self.yy],font=(self.text_familly,self.label_text_size,self.font_style),width=8))
+        self.date[2].current(1)
+        self.date[2].grid(row=0,column=2)
+        frame_date_combo_box.grid(row=0,column=1,padx=10)
+        frame_date.pack(pady=15)
+        self.canvas.create_window(self.width*4/10,self.height*9/20+self.padding*3/4,window=frame_form)
+        #bind the ComboBox
+        self.date[0].bind("<Button-1>",self.jj_bind)
+        self.date[1].bind("<Button-1>",self.mm_bind)
+        self.date[2].bind("<Button-1>",self.yy_bind)
+        for combobox in self.date:
+            combobox.bind("<FocusIn>",self.focus_in_combobox)
+            combobox.bind("<FocusOut>",self.focus_out_combobox)
         #bind the enties to get position
         self.entries_form[0].bind("<Button-1>",self.entry_0)
         self.entries_form[1].bind("<Button-1>",self.entry_1)
@@ -188,11 +204,19 @@ class MyApp:
         self.entries_form[7].bind("<Button-1>",self.entry_7)
         #bind the enties
         for index in range(len(self.entries_form)):
-                self.entries_form[index].bind("<FocusIn>",self.focus_in_entry)
-                self.entries_form[index].bind("<FocusOut>",self.focus_out_entry)
-                self.entries_form[index].bind("<Up>",self.up_entry)
-                self.entries_form[index].bind("<Down>",self.down_entry)
-
+            self.entries_form[index].bind("<FocusIn>",self.focus_in_entry)
+            self.entries_form[index].bind("<FocusOut>",self.focus_out_entry)
+            self.entries_form[index].bind("<Up>",self.up_entry)
+            self.entries_form[index].bind("<Down>",self.down_entry)
+        #create buttons
+        button_menu=Button(self.canvas,fg=self.button_C_color,bg=self.button_C_background_color,font=(self.text_familly,self.text_C_size,self.font_style),text="Menu",width=17,command=self.return_to_menu)
+        button_calculer=Button(self.canvas,fg=self.button_C_color,bg=self.button_C_background_color,font=(self.text_familly,self.text_C_size,self.font_style),text="Calculer",width=17)
+        button_ajouter_image=Button(self.canvas,fg=self.button_C_color,bg=self.button_C_background_color,font=(self.text_familly,self.text_C_size,self.font_style),text="Ajouter une image",width=17)
+        button_supprimer_image=Button(self.canvas,fg=self.button_C_color,bg=self.button_C_background_color,font=(self.text_familly,self.text_C_size,self.font_style),text="supprimer une image",width=17)
+        self.canvas.create_window(self.width/8,self.height*17/18,window=button_menu)
+        self.canvas.create_window(self.width*3/8,self.height*17/18,window=button_calculer)
+        self.canvas.create_window(self.width*5/8,self.height*17/18,window=button_ajouter_image)
+        self.canvas.create_window(self.width*7/8,self.height*17/18,window=button_supprimer_image)
     #bind's functions to get position
     def entry_0(self,*args):
         self.next_focus_entry=0
@@ -210,7 +234,12 @@ class MyApp:
         self.next_focus_entry=6
     def entry_7(self,*args):
         self.next_focus_entry=7
-        
+    def jj_bind(self,*args):
+        self.index_next_focus_combobox=0
+    def mm_bind(self,*args):
+        self.index_next_focus_combobox=1
+    def yy_bind(self,*args):
+        self.index_next_focus_combobox=2
     #bind's functions
     def focus_in_entry(self,*args)->None:
         self.current_focus_entry,self.next_focus_entry=self.next_focus_entry,-1
@@ -234,7 +263,46 @@ class MyApp:
     def down_entry(self,*args):
         self.next_focus_entry=(self.current_focus_entry+1)%len(self.entries_form)
         self.entries_form[self.next_focus_entry].focus()
+    def focus_in_combobox(self,*args):
+        self.index_focus_combobox,self.index_next_focus_combobox=self.index_next_focus_combobox,-1
+        try :
+            self.current_dd_mm_yy[self.index_focus_combobox]=int(self.date[self.index_focus_combobox].get())
+        except :
+            self.insert_date_combobox(self.index_focus_combobox)
+        for i in range(3):
+            self.insert_date_combobox(i)
+    def focus_out_combobox(self,*args):
+        try:
+            value=int(self.date[self.index_focus_combobox].get())
+            if self.index_focus_combobox==0 and value not in range(1,32):
+                raise ValueError("le jour devrait etre entre 1 et 31 !!")
+            elif self.index_focus_combobox==1 and value not in range(1,13):
+                raise ValueError("le mois devrait etre entre 1 et 12 !!")
+            elif self.index_focus_combobox==2 and value!=self.yy-1 and value!=self.yy:
+                raise ValueError(f"l'année' devrait etre entre {self.yy-1} ou {self.yy} !!")
+            self.current_dd_mm_yy[self.index_focus_combobox]=value
+        except :
+            self.insert_date_combobox(self.index_focus_combobox)
+        for i in range(3):
+            self.insert_date_combobox(i)
+    #button's functions
+    def button_calculer(self):
+        self.clean_window()
+        self.create_form()
+    def return_to_menu(self):
+        self.next_focus_entry=(self.current_focus_entry+1)%len(self.entries_form)
+        self.can_it_be_number(self.entries_form[self.current_focus_entry].get())
+        self.clean_window()
+        self.create_menu()
     #other
+    def insert_date_combobox(self,index):
+        if index in range(2):
+            self.date[index].current(self.current_dd_mm_yy[index]-1)
+        elif index==2:
+            self.date[index].current(1-self.yy+self.current_dd_mm_yy[index])
+    def clean_window(self):
+        self.canvas.destroy()
+        self.create_canvas()
     def can_it_be_number(self,string_to_check:str)->bool:
         try :
             number=float(string_to_check)
@@ -245,12 +313,10 @@ class MyApp:
                 self.data_entries[self.current_focus_entry]=number
                 self.entries_form[self.current_focus_entry].delete(0,END)
                 self.entries_form[self.current_focus_entry].insert(0,number)
-                print(self.data_entries)
             return True 
         except:
             if self.next_focus_entry!=self.current_focus_entry and self.current_focus_entry in range(len(self.entries_form)) and self.next_focus_entry in range(len(self.entries_form)) :
                 self.data_entries[self.current_focus_entry]=-1
-                print(self.data_entries)
             return False
 if __name__=="__main__" :
     app=MyApp()
